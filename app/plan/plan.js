e350d8b778338d8683e6accbfcb438329037493c
norheim <norheim@mit.edu>
2017-11-11 14:54:00 -1000

    updated plan

--- app/plan/plan.js
+++ app/plan/plan.js
@@ -213,7 +213,7 @@ class PlanManager {
     }
 
     _fetchPlan() {
-        let currentPlanUrl = this.hostname + '/sextant_plans/hawaii_sextant_day5_path1.json';
+        let currentPlanUrl = this.hostname + '/sextant_plans/KIL1004_A_EVA_A_plan.json';
         $.getJSON(currentPlanUrl, this.planFromJSON.bind(this));
     }
 	
